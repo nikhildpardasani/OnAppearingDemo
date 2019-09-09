8bce84e21d2e60debeaf205506b43712d0082b70
nikhildpardasani <51998596+nikhildpardasani@users.noreply.github.com>
2019-09-09 12:26:08 +0530

    Add files via upload

---
+++ OnAppearingDemo/OnAppearingDemo/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using OnAppearingDemo.Models;
+
+namespace OnAppearingDemo.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
---
+++ OnAppearingDemo/OnAppearingDemo/Views/ItemsPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using OnAppearingDemo.Models;
+using OnAppearingDemo.Views;
+using OnAppearingDemo.ViewModels;
+
+namespace OnAppearingDemo.Views
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class ItemsPage : ContentPage
+    {
+        ItemsViewModel viewModel;
+
+        public ItemsPage()
+        {
+            InitializeComponent();
+
+            BindingContext = viewModel = new ItemsViewModel();
+            Btn1.Clicked += Btn1_Clicked;
+        }
+
+        private void Btn1_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Page2());
+        }
+
+        //I need to call this Method whenever this Page Appears(Navigation.PushAsync() or Navigation.PopAsync() is called.)
+        //This is not fired when I return back from a Page2 in Android but it does in iOS.
+        //Hence I am stuck Because for iOS this works well but Android doesn't.
+        //Put a breakpoint on this method and observe the behaviour in both Android and iOS.
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            if (viewModel.Items.Count == 0)
+                viewModel.LoadItemsCommand.Execute(null);
+        }
+    }
+}
